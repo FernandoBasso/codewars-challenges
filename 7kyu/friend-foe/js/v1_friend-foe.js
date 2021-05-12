111c5ee4a2245e769bfff66d94e164272e1bafd3
Fernando Basso <fernandobasso.br@gmail.com>
2021-05-12 11:06:09 -0300

    Refactor friend or foe unit tests

--- 7kyu/friend-foe/js/v1_friend-foe.js
+++ 7kyu/friend-foe/js/v1_friend-foe.js
@@ -1,5 +1,3 @@
-const expect = require('expect');
-
 const REQUIRED_LENGHT = 4;
 
 // /**
@@ -25,28 +23,7 @@ const REQUIRED_LENGHT = 4;
  * @param {Array<string>} names
  * @return {Array<string>}
  */
-function friend(names) {
+export function friend(names) {
   return names.filter(name => name.length === REQUIRED_LENGHT);
-}
-
-expect(friend([])).toEqual([]);
-expect(friend(['K', 'Mu', 'Mia'])).toEqual([]);
-
-expect(friend(['Goku', 'Lara'])).toEqual(['Goku', 'Lara']);
-
-expect(
-  friend(['Ryan', 'Kieran', 'Mark'])
-).toEqual(['Ryan', 'Mark']);
-
-expect(
-  friend(['Ryan', 'Jimmy', '123', '4', 'Cool Man'])
-).toEqual(['Ryan']);
-
-expect(
-  friend(['Jimm', 'Cari', 'aret', 'truehdnviegkwgvke', 'sixtyiscooooool'])
-).toEqual(['Jimm', 'Cari', 'aret']);
-
-expect(
-  friend(['Love', 'Your', 'Face', '😄'])
-).toEqual(['Love', 'Your', 'Face']);
+};
 
