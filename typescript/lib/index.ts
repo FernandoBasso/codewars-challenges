b12922e154d0d73ac81173f89c603857b748bf01
Fernando Basso <fernandobasso.br@gmail.com>
2022-05-15 12:39:31 -0300

    typescript(lib): Add toUpper utility function

--- typescript/lib/index.ts
+++ typescript/lib/index.ts
@@ -1,4 +1,8 @@
 export type Nullable<Type> = Type | undefined | null;
 
 export { englishAlphabetLower, englishAlphabetUpper } from "./alphabet.ts";
+
 export { toLower } from "./toLower.ts";
+export { toUpper } from "./toUpper.ts";
+
+export { isEven } from "./isEven.ts";
